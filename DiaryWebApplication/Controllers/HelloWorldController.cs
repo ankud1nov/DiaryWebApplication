1faaf3ccfe1ff0d0c04a86341c07dfa85948b8b3
Ilya Ankudinov <Ilya.Ankudinov@russianpost.ru>
2021-07-20 18:51:39 +0400

    Добавьте файлы проекта.

---
+++ DiaryWebApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiaryWebApplication.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
